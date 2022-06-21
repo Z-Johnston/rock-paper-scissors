b25642868f5775ccc2b6b5bfa6e2f920bb80ded8
Zack Johnston <zackjohnston@me.com>
2022-06-21 17:31:29 -0500

    defining the playRound function

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ function computerPlay() {
     return rockPaperScissors[parseInt(Math.floor(Math.random()*3))]
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return "you lose, paper beats rock"
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "you win, rock beats scissors"
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "you win, paper beats rock"
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "you lose, scissors beats paper"
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "you win, scissors beats paper"
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "you lose, rock beats scissors"
+    } else {
+        return "it is a tie"
+    }
+}
+
+
+playerSelection = prompt("Enter rock, paper or scissors").toLocaleLowerCase();
+computerSelection = computerPlay()
+
+console.log(playRound(playerSelection, computerSelection))
 
 
